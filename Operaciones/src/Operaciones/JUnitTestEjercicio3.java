f5ce95d7ddcbe2fbf517b09820f7355d0eda842d
JerrySalazar07 <56902411+JerrySalazar07@users.noreply.github.com>
2023-10-22 19:08:09 -0500

    Update JUnitTestEjercicio3.java

--- Operaciones/src/Operaciones/JUnitTestEjercicio3.java
+++ Operaciones/src/Operaciones/JUnitTestEjercicio3.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class JUnitTestEjercicio3 {
 
+	//Encontrar el número mayor con array vacía
 	@Test
     public void testFindMaxNumberWithEmptyArray() {
         int[] arr = {};
@@ -14,21 +15,21 @@ public class JUnitTestEjercicio3 {
             Operaciones.findMaxNumber(arr);
         });
     }
-	
+	//Encontrar el número mayor con un solo elemento en la array
 	@Test
     public void testFindMaxNumberWithSingleElementArray() {
         int[] arr = {5};
         int result = Operaciones.findMaxNumber(arr);
         assertEquals(5, result);
     }
-	
+	//Encontrar el número mayor con varios elementos en la array
 	@Test
     public void testFindMaxNumberWithMultipleElementArray() {
         int[] arr = {2, 5, 3, 8, 1};
         int result = Operaciones.findMaxNumber(arr);
         assertEquals(8, result);
     }
-	
+	//Encontrar el número mayor entre números negativos
 	@Test
     public void testFindMaxNumberWithNegativeElements() {
         int[] arr = {-2, -5, -3, -8, -1};
