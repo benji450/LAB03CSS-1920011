3689f473f1dd0172dfd6863b7cf849425b5f04a9
JerrySalazar07 <56902411+JerrySalazar07@users.noreply.github.com>
2023-10-22 19:06:08 -0500

    Update JUnitTestEjercicio2.java

--- Operaciones/src/Operaciones/JUnitTestEjercicio2.java
+++ Operaciones/src/Operaciones/JUnitTestEjercicio2.java
@@ -5,25 +5,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class JUnitTestEjercicio2 {
 
-	
+	//Cuando el primer número es el mayor
 	@Test
     public void testFindHighestNumberWithN1Greater() {
         int result = Operaciones.findHighestNumber(5, 3, 2);
         assertEquals(5, result);
     }
-	
+	//Cuando el tercer número es el mayor
 	@Test
     public void testFindHighestNumberWithN3Greater() {
         int result = Operaciones.findHighestNumber(2, 3, 5);
         assertEquals(5, result);
     }
-	
+	//Cuando el segundo número es el mayor
 	@Test
 	    public void testFindHighestNumberWithN2Greater() {
 	        int result = Operaciones.findHighestNumber(2, 5, 3);
 	        assertEquals(5, result);
 	    }
-	 
+	//Cuando todos los números son iguales 
 	@Test
 	    public void testFindHighestNumberWithEqualNumbers() {
 	        int result = Operaciones.findHighestNumber(2, 2, 2);
