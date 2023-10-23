8b47db6f72d43c24eeccdbf6a6db28c6e140c57d
JerrySalazar07 <56902411+JerrySalazar07@users.noreply.github.com>
2023-10-22 19:10:04 -0500

    Update JUnitTestEjercicio4.java

--- Operaciones/src/Operaciones/JUnitTestEjercicio4.java
+++ Operaciones/src/Operaciones/JUnitTestEjercicio4.java
@@ -4,24 +4,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JUnitTestEjercicio4 {
+
+	//Calcular cubo con cero
 	 @Test
 	    public void testCalculateCubeWithZero() {
 	        int result = Operaciones.calculateCube(0);
 	        assertEquals(0, result);
 	    }
-	 
+	 //Calcular cubo usando un número positivo
 	 @Test
 	    public void testCalculateCubeWithPositiveNumber() {
 	        int result = Operaciones.calculateCube(5);
 	        assertEquals(125, result);
 	    }
-	 
+	 //Calcular usando un número negativo
 	 @Test
 	    public void testCalculateCubeWithNegativeNumber() {
 	        int result = Operaciones.calculateCube(-5);
 	        assertEquals(-125, result);
 	    }
-	 
+	 //Calcular usando el mayor valor posible de tipo int
 	 @Test
 	    public void testCalculateCubeWithMaxInt() {
 	        int result = Operaciones.calculateCube(Integer.MAX_VALUE);
