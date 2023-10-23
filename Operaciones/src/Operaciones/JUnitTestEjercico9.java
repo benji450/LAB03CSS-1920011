a59ae914d21c7f9739c2b6c27666304d61fbdf08
JerrySalazar07 <56902411+JerrySalazar07@users.noreply.github.com>
2023-10-22 19:18:06 -0500

    Update JUnitTestEjercico9.java

--- Operaciones/src/Operaciones/JUnitTestEjercico9.java
+++ Operaciones/src/Operaciones/JUnitTestEjercico9.java
@@ -7,25 +7,26 @@ import org.junit.jupiter.api.Test;
 
 class JUnitTestEjercico9 {
 
+	//Dividor dos números enteros positivos con resultado entero
 	@Test
     public void testDivTwoNumbersWithIntegers() {
         double result = Operaciones.divTwoNumbers(10, 2);
         assertEquals(5.0, result, 0.0);
     }
-	
+	//Dividir un número entero positivo sobre cero
 	@Test
     public void testDivTwoNumbersWithDivisorZero() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             Operaciones.divTwoNumbers(10, 0);
         });
     }
-	
+	//Dividir dos números enteros positivos con resultado decimal
 	@Test
     public void testDivTwoNumbersWithFloatingPointNumber() {
         double result = Operaciones.divTwoNumbers(10, 3);
         assertEquals(3.3333333333333335, result, 0.000000000000001);
     }
-	
+	//Dividir dos números decimales positivos
 	@Test
     public void testDivTwoNumbersWithTwoFloatingPointNumbers() {
         double result = Operaciones.divTwoNumbers(10.5, 2.5);
